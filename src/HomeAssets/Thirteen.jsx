fd0fdf15ad9f057f7c8a072b8a60b8f819975408
Jayeshbagul <jbagul366@gmail.com>
2024-09-03 13:58:27 +0530

    lenskart

--- src/HomeAssets/Thirteen.jsx
+++ src/HomeAssets/Thirteen.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Import local image
+import ContactImage from 'src/assets/contact.jpg';
+
 const Thirteen = () => {
     return (
         <>
@@ -12,17 +15,29 @@ const Thirteen = () => {
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//no-resize/a/i/aquacolor-dusk-blue-premium-zero-power-monthly_dusk_blue_kiara_01_09_2023.jpg" className="img-fluid" alt="Contact Lens 1" />
+                            <img
+                                src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//no-resize/a/i/aquacolor-dusk-blue-premium-zero-power-monthly_dusk_blue_kiara_01_09_2023.jpg"
+                                className="img-fluid"
+                                alt="Contact Lens 1"
+                            />
                         </Link>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//no-resize/a/i/aquacolor-gray-premium-zero-power-monthly_gray_kiara_01_09_2023.jpg" className="img-fluid" alt="Contact Lens 2" />
+                            <img
+                                src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//no-resize/a/i/aquacolor-gray-premium-zero-power-monthly_gray_kiara_01_09_2023.jpg"
+                                className="img-fluid"
+                                alt="Contact Lens 2"
+                            />
                         </Link>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//no-resize/a/i/aquacolor-silver-gray-premium-zero-power-monthly_silver_grey_kiara_01_09_2023.jpg" className="img-fluid" alt="Contact Lens 3" />
+                            <img
+                                src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//no-resize/a/i/aquacolor-silver-gray-premium-zero-power-monthly_silver_grey_kiara_01_09_2023.jpg"
+                                className="img-fluid"
+                                alt="Contact Lens 3"
+                            />
                         </Link>
                     </div>
                 </div>
@@ -31,7 +46,11 @@ const Thirteen = () => {
             <div className="container-fluid mt-5 mb-3">
                 <div className="row">
                     <div className="col-12">
-                        <img src="src/assets/contact.jpg" className="d-block w-100 img-fluid" alt="Contact Information" />
+                        <img
+                            src={ContactImage}
+                            className="d-block w-100 img-fluid"
+                            alt="Contact Information"
+                        />
                     </div>
                 </div>
             </div>

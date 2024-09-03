41f6f1946766e346c262049613ecfd60595f5be1
Jayeshbagul <jbagul366@gmail.com>
2024-09-03 13:55:51 +0530

    lenkart

--- src/HomeAssets/TwelCon.jsx
+++ src/HomeAssets/TwelCon.jsx
@@ -12,17 +12,17 @@ const TwelCon = () => {
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="src/assets/eye 2.1.webp" className="img-fluid" alt="Eyeglass 1" />
+                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-e10235-c7-eyeglasses_g_2375_image_pla.jpg" className="img-fluid" alt="Eyeglass 1" />
                         </Link>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="src/assets/eye 2.2.webp" className="img-fluid" alt="Eyeglass 2" />
+                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-e11675-c2-eyeglasses_g_0134.jpg" className="img-fluid" alt="Eyeglass 2" />
                         </Link>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="src/assets/eye 2.3.webp" className="img-fluid" alt="Eyeglass 3" />
+                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-rimless-jj-e12565-c2-eyeglasses_john-jacobs-rimless-jj-e12565-c2-eyeglasses_g_3200.jpg" className="img-fluid" alt="Eyeglass 3" />
                         </Link>
                     </div>
                 </div>
@@ -33,17 +33,17 @@ const TwelCon = () => {
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="src/assets/sun 1.webp" className="img-fluid" alt="Sunglass 1" />
+                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/i/grey-gradient-black-full-rim-wayfarer-john-jacobs-jj-tints-jj-s13227-c4-sunglasses_csvfile-1708406002394-img_7007.jpg" className="img-fluid" alt="Sunglass 1" />
                         </Link>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="src/assets/sun 2.webp" className="img-fluid" alt="Sunglass 2" />
+                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-s12808-c2-sunglasses_g_8841.jpg" className="img-fluid" alt="Sunglass 2" />
                         </Link>
                     </div>
                     <div className="col-12 col-sm-6 col-md-4 mb-3">
                         <Link to="/product">
-                            <img src="src/assets/sun 3.webp" className="img-fluid" alt="Sunglass 3" />
+                            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-s12808-c1-sunglasses_g_8864_image_pla.jpg" className="img-fluid" alt="Sunglass 3" />
                         </Link>
                     </div>
                 </div>
@@ -51,7 +51,7 @@ const TwelCon = () => {
             <div className="container mt-5 mb-5">
                 <div className="row">
                     <div className="col-12">
-                        <img src="src/assets/hopper.webp" className="d-block w-100 img-fluid" alt="Additional Content" />
+                        <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-s12808-c1-sunglasses_g_8864_image_pla.jpg" className="d-block w-100 img-fluid" alt="Additional Content" />
                     </div>
                 </div>
             </div>

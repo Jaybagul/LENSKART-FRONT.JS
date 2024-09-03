e8d159f62459a715138e8affdca81e13bdb75380
Jayeshbagul <jbagul366@gmail.com>
2024-09-03 13:59:53 +0530

    lenskart

--- src/HomeAssets/Eleventh.jsx
+++ src/HomeAssets/Eleventh.jsx
@@ -8,7 +8,7 @@ const Eleventh = () => {
   return (
     <>
 
-      <div className="container">
+      {/* <div className="container">
         <div className="row">
           <div className="col-3"><img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-e10235-c7-eyeglasses_g_2375_image_pla.jpg" alt="" /></div>
           <div className="col-3"><img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-e11675-c2-eyeglasses_g_0134.jpg" alt="" /></div>
@@ -22,7 +22,7 @@ const Eleventh = () => {
           <div className="col-3"><img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-s12808-c2-sunglasses_g_8841.jpg" alt="" /></div>
           <div className="col-3"><img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//j/o/john-jacobs-jj-s12808-c1-sunglasses_g_8864_image_pla.jpg" alt="" /></div>
         </div>
-      </div>
+      </div> */}
 
     </>
   );

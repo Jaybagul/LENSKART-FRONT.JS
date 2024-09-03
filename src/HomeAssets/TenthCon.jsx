b6ce87e16e81a13279e7c2407f22dde273f9066c
Jayeshbagul <jbagul366@gmail.com>
2024-09-03 13:44:12 +0530

    lenskart

--- src/HomeAssets/TenthCon.jsx
+++ src/HomeAssets/TenthCon.jsx
@@ -12,10 +12,10 @@ const TenthCon = () => {
       <div className="container mt-5 mb-5">
         <div className="row">
           <div className="col-md-6 col-sm-12 p-2">
-            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//l/i/lenskart-air-la-e15417-w-c1-eyeglasses_csvfile-1701688470121-img_7893.jpg" className="d-block w-100 img-fluid" alt="Image 1" />
+            <img src="https://static1.lenskart.com/media/desktop/img/republic/celb-new/call.png" className="d-block w-100 img-fluid" alt="Image 1" />
           </div>
           <div className="col-md-6 col-sm-12 p-2">
-            <img src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//l/i/lenskart-air-la-e15417-w-c1-eyeglasses_csvfile-1701688470121-img_7893.jpg" className="d-block w-100 img-fluid" alt="Image 2" />
+            <img src="https://static1.lenskart.com/media/desktop/img/republic/celb-new/call.png" className="d-block w-100 img-fluid" alt="Image 2" />
           </div>
         </div>
         <div className="row mt-3">

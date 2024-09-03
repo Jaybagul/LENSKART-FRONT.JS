3be9f2319cf989bc273b9e9a439f73007bb99911
Jayeshbagul <jbagul366@gmail.com>
2024-09-03 13:48:38 +0530

    lenskart

--- src/HomeAssets/Eleventh.jsx
+++ src/HomeAssets/Eleventh.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Import local images
+import Eyeglass1 from 'src/assets/eyeglass1.webp';
+import Eyeglass2 from 'src/assets/eyeglass2.webp';
+import Eyeglass3 from 'src/assets/eyeglass3.webp';
+import Sunglass1 from 'src/assets/sunglass1.webp';
+import Sunglass2 from 'src/assets/sunglass2.webp';
+import Sunglass3 from 'src/assets/sunglass3.webp';
+import AdditionalImage from 'src/assets/eleventh.webp';
+
 const Eleventh = () => {
-  const eyeglassesImages = [
-    'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//l/i/lenskart-air-la-e15417-w-c1-eyeglasses_csvfile-1701688470121-img_7893.jpg',
-    'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//l/i/lenskart-air-la-e15417-w-c1-eyeglasses_csvfile-1701688470121-img_7893.jpg',
-    'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//l/i/lenskart-air-la-e15417-w-c1-eyeglasses_csvfile-1701688470121-img_7893.jpg'
-  ];
-
-  const sunglassesImages = [
-    'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//v/i/silver-black-brown-full-rim-rectangle-square-vincent-chase-online-the-metal-edit-vc-s12593-p-c3-polarized-sunglasses_csvfile-1687346118685-g_2547.jpg',
-    'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//v/i/black-green-full-rim-wayfarer-vincent-chase-athleisure-vc-s14525-c2-sunglasses_g_8300.jpg',
-    'https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/371x178/9df78eab33525d08d6e5fb8d27136e95//v/i/black-grey-full-rim-wayfarer-shape-vincent-chase-style-cast-pc-vc-s11290-c3-polarized-sunglasses_vincent-chase-vc-s11290-c3-sunglasses_sunglasses__j_7431_1_5july23.jpg'
-  ];
-
   return (
     <>
+      {/* Eyeglasses and Sunglasses Section */}
       <div className="container mb-5" style={{ margin: 'auto', width: '80%' }}>
 
+        {/* Eyeglasses Section */}
         <div className="row mt-5 mb-3">
           <div className="col-12">
             <h4 className="text">EYEGLASSES</h4>
             <hr />
           </div>
-          {eyeglassesImages.map((img, index) => (
+          {[Eyeglass1, Eyeglass2, Eyeglass3].map((img, index) => (
             <div key={index} className="col-12 col-sm-6 col-md-4 mb-4">
               <Link to="/product">
                 <img
@@ -36,12 +35,13 @@ const Eleventh = () => {
           ))}
         </div>
 
+        {/* Sunglasses Section */}
         <div className="row mt-5 pt-2">
           <div className="col-12">
             <h4 className="text">SUNGLASSES</h4>
             <hr />
           </div>
-          {sunglassesImages.map((img, index) => (
+          {[Sunglass1, Sunglass2, Sunglass3].map((img, index) => (
             <div key={index} className="col-12 col-sm-6 col-md-4 mb-4">
               <Link to="/product">
                 <img
@@ -60,28 +60,13 @@ const Eleventh = () => {
         <div className="row">
           <div className="col-12">
             <img
-              src="https://static1.lenskart.com/media/desktop/img/Nov22/Updated%20brand%20banner%20jj%20.jpg"
+              src={AdditionalImage}
               className="img-fluid d-block w-100"
               alt="Additional Content"
             />
           </div>
         </div>
       </div>
-
-      <script>
-        const eyeglassesImages = [
-        `${process.env.PUBLIC_URL}/images/eyeglass1.jpg`,
-        `${process.env.PUBLIC_URL}/images/eyeglass2.jpg`,
-        `${process.env.PUBLIC_URL}/images/eyeglass3.jpg`
-        ];
-
-        const sunglassesImages = [
-        `${process.env.PUBLIC_URL}/images/sunglass1.jpg`,
-        `${process.env.PUBLIC_URL}/images/sunglass2.jpg`,
-        `${process.env.PUBLIC_URL}/images/sunglass3.jpg`
-        ];
-
-      </script>
     </>
   );
 };
